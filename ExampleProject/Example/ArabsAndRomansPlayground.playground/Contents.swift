f0753c7b6b6d24388f1296db623e7e28a21c9f7c
bagofstars <glprog@gmail.com>
2015-12-15 17:46:21 +0000

    tweaked playground content

--- ExampleProject/Example/ArabsAndRomansPlayground.playground/Contents.swift
+++ ExampleProject/Example/ArabsAndRomansPlayground.playground/Contents.swift
@@ -4,13 +4,11 @@
 //Below you can try converting different arabic integers into roman numbers in a string format.
 //Supported range of numbers is [1, 3999]
 
-var str = try! ArabicToRoman.romanNumberFromArabicNumber(3999)
+var str = try! ArabicToRoman.romanNumberFromArabicNumber(1689)
 
 
 
 //Here you can try converting strings which represents roman numbers into arabic numbers.
-//All numbers between I and MMMCMXCIX are supported. Lowercase and uppercase characters are acceptable.
+//All numbers between I and MMMCMXCIX are supported. Both lowercase and uppercase characters are acceptable.
 
 var resultArabicNumber = try! RomanToArabic.arabicNumberFromRomanNumber("MMcDXcvIi")
-
- 
